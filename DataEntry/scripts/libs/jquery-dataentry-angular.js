4a3cf6fd87bf10736628af640bab6c712d57672d
ybhalani <yashbhalani88@gmail.com>
2016-07-12 15:09:14 -0700

    yash

---
+++ DataEntry/scripts/libs/jquery-dataentry-angular.js
@@ -0,0 +1,43 @@
+
+
+angular.module('ug.dataentry', [])
+.directive('dataentry', ["$compile", function ($compile) {
+  
+  var linker = function (scope, el, attrs) {
+    // extend the scope with functions for the form validation
+    var element = el[0];
+    
+    if (scope.dataentry) throw "the scope has already a [dataentry] property. Only one dataentry per scope is supported";
+    if (!scope.schema) throw "no schema defined inside the scope";
+    
+    // supports only one dataentry per scope
+    scope.dataentry = new $.Forms.DataEntry({
+      $el: $(element),
+      schema: scope.schema,
+      context: scope
+    });
+    
+    _.extend(scope, {
+      validate: function (params) {
+        return this.dataentry.validate(params);
+      }
+    });
+
+    //trigger focus on the first active input element
+    _.delay(function () {
+      el.find(":input:not(:disabled):not([readonly]):first").trigger('focus');
+    }, 200);
+
+    //bind event handler to unbind events
+    scope.$on("$destroy", function() {
+      //remove validation and undelegate events
+      scope.dataentry.removeValidation().undelegateEvents();
+    });
+  };
+  
+  return {
+    restrict: 'A',
+    replace: false,
+    link: linker
+  };
+}]);
---
+++ ScrollDetector/README.md
@@ -0,0 +1,10 @@
+# JQueryScrollDetector
+
+```javascript
+	$(document).on('scrollUp',function(){
+        alert("scroll up event");
+    });
+	$(document).on('scrollDown',function(){
+        alert("scroll down event");
+    });
+```
